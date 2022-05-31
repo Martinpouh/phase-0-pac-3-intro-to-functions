8948a031c4cfb5a69bf51d562068c86ab8f4159f
Martinpouh <martinchege2015@gmail.com>
2022-05-31 19:45:39 +0300

    commit tree 2c939d45cd3e0a1fda99907342b9ab56cc6a8c7e

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Follow along with the examples here
+
+function sayHelloToMartin() {
+    console.log("Hello, Martin!");
+}
+
+
+function sayHelloToChege() {
+    console.log("Hello, Chege!");
+}
+
+
+function sayHelloToMacharia() {
+    console.log("Hello, Macharia!");
+}
+sayHelloToMacharia();
+sayHelloToMartin();
+sayHelloToChege();
+
+
+
+
+
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+sayHelloTo("Macharia");
+sayHelloTo("Martin");
+sayHelloTo("Chege");
+
+
+function add(x, y) {
+    return x + y;
+}
+console.log(add(10.5, 2));
+
+function say(greeting, firstName) {
+    console.log("I was called");
+    return `${greeting}, ${firstName}!`;
+}
